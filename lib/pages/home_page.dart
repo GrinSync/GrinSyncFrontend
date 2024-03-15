e1dfd0fad9b1fac6e5d4e34bb006e5c76af0d85a
彭羽鑫 <kevinpyx@163.com>
2024-03-15 11:27:31 -0500

    started homepage implementation

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // create event dictionary
+  // event fields are on trello
+
+}
