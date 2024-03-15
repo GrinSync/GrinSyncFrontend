5a3d6a213cdf2f8d2d44c143174fa0e8a7c2df2c
bradleyramsey <bradley.ramsey01@gmail.com>
2024-03-15 11:04:00 -0500

    Added documentation to user_authentication.dart

--- lib/api/user_authorization.dart
+++ lib/api/user_authorization.dart
@@ -6,24 +6,26 @@ import 'package:http/http.dart' as http;
 
 
 Future userAuthentication(String username, String password) async {
+  // body stores the username and password in a single map data structure
   Map body = {
     'username': username,
     'password': password,
   };
-  var url = Uri.parse('http://grinsync.com/api/auth');
-  //var stuff = http.get(url);
-  //final csrfToken = await getCsrfToken();
+  var url = Uri.parse('http://grinsync.com/api/auth'); // url to send info
   var result = await http.post(url, body: body);
-  print(result.body);
+  print(result.body); // This is for testing purposes only
   Map json = jsonDecode(result.body);
+  // Check if Django returned a user or login failed error
   if (json.containsKey("non_field_errors")){
-    return 'yum';
+    return 'Login Failed';
   }
+  // If the login was successful, we can store the login token for future use
   if (result.statusCode == 200) {
     Map json = jsonDecode(result.body);
     String token = json['token'];
     var box = await Hive.openBox(tokenBox);
-    box.put('token', token);
+    // TODO: Encrypt this box!
+    box.put('token', token); // save token in box
     box.close();
   }
 }
