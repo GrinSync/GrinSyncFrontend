2fb8927e22ff61c8be7d8f99f8f0ee1549f68c8f
bradleyramsey <bradley.ramsey01@gmail.com>
2024-03-18 20:54:40 -0500

    Changed login to work with https and include error messages

--- lib/api/user_authorization.dart
+++ lib/api/user_authorization.dart
@@ -4,21 +4,16 @@ import 'package:flutter_test_app/constants.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:http/http.dart' as http;
 
-
 Future userAuthentication(String username, String password) async {
   // body stores the username and password in a single map data structure
   Map body = {
     'username': username,
     'password': password,
   };
-  var url = Uri.parse('http://grinsync.com/api/auth'); // url to send info
+  var url = Uri.parse('https://grinsync.com/api/auth'); // url to send info
   var result = await http.post(url, body: body);
   print(result.body); // This is for testing purposes only
-  Map json = jsonDecode(result.body);
-  // Check if Django returned a user or login failed error
-  if (json.containsKey("non_field_errors")){
-    return 'Login Failed';
-  }
+
   // If the login was successful, we can store the login token for future use
   if (result.statusCode == 200) {
     Map json = jsonDecode(result.body);
@@ -27,5 +22,15 @@ Future userAuthentication(String username, String password) async {
     // TODO: Encrypt this box!
     box.put('token', token); // save token in box
     box.close();
+    await getUser(token);
   }
+  else{
+     return 'Login Failed';
+  }
+}
+
+getUser(String token) async{
+  var url = Uri.parse('http://grinsync.com/api/getUser'); // url to send info
+  var res = await http.get(url, headers: {});
+  print(res.body);
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -29,72 +29,92 @@ class _LoginPage extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('GrinSync Login'),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
+        appBar: AppBar(
+          title: const Text('GrinSync Login'),
+        ),
+        body: SingleChildScrollView(
+            child: Container(
           padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-        TextField(
-          controller: _email,
-          keyboardType: TextInputType.emailAddress,
-          autocorrect: false,
-          enableSuggestions: false,
-          decoration: const InputDecoration(
-            prefixIcon: Icon(Icons.person_outline_outlined),
-            labelText: 'Email',
-            hintText: 'Enter your email',
-            border: OutlineInputBorder()
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            TextField(
+              controller: _email,
+              keyboardType: TextInputType.emailAddress,
+              autocorrect: false,
+              enableSuggestions: false,
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.person_outline_outlined),
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                  border: OutlineInputBorder()),
             ),
-        ),
-        const SizedBox(height: 10),
-        TextField(
-          controller: _password,
-          autocorrect: false,
-          obscureText: true,
-          enableSuggestions: false,
-          decoration: const InputDecoration(
-            prefixIcon: Icon(Icons.fingerprint),
-            labelText: 'Password',
-            hintText: 'Enter your password',
-            border: OutlineInputBorder(),
-            // suffixIcon: IconButton(
-            //   onPressed: null,
-            //   icon: Icon(Icons.remove_red_eye_sharp)
-            // )
-          ),
-        ),
-        const SizedBox(height: 10),
-        Align(
-          alignment: Alignment.centerRight,
-          child: TextButton(
-            onPressed: (){},
-            child: const Text('Forgot Password?')
-          )
-        ),
-        Align(
-          alignment: Alignment.center,
-        child: ElevatedButton(
-            onPressed: () async {
-              var auth = await userAuthentication(_email.text, _password.text);
-              if (auth.runtimeType == String){
-                print("These Login credentials are invalid");
-              }
-              else{
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const MyApp()),
-          );
-              }
-            },
-            child: const Text('Log in')),
-        )
-      ]),
-      )
-      )
-    );
+            const SizedBox(height: 10),
+            TextField(
+              controller: _password,
+              autocorrect: false,
+              obscureText: true,
+              enableSuggestions: false,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.fingerprint),
+                labelText: 'Password',
+                hintText: 'Enter your password',
+                border: OutlineInputBorder(),
+                // suffixIcon: IconButton(
+                //   onPressed: null,
+                //   icon: Icon(Icons.remove_red_eye_sharp)
+                // )
+              ),
+            ),
+            const SizedBox(height: 10),
+            Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                    onPressed: () {}, child: const Text('Forgot Password?'))),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.yellow),
+                  onPressed: () async {
+                    var auth =
+                        await userAuthentication(_email.text, _password.text);
+                    if (auth.runtimeType == String) {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text('Login Failed'),
+                            content: const SingleChildScrollView(
+                              child: ListBody(
+                                children: <Widget>[
+                                  Text(
+                                      'The Lgin credentials provided do not match a user.'),
+                                  Text(
+                                      'Please re-enter your credentials and try again.'),
+                                ],
+                              ),
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                child: const Text('Okay'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const MyApp()),
+                      );
+                    }
+                  },
+                  child: const Text('Log in')),
+            )
+          ]),
+        )));
   }
 }
