579c7e1147611a4dbb71f309450a75de9a54be58
bradleyramsey <bradley.ramsey01@gmail.com>
2024-04-01 11:40:51 -0500

    Checking for different error messages

--- lib/api/new_event_info.dart
+++ lib/api/new_event_info.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter_test_app/constants.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:http/http.dart' as https;
@@ -28,6 +30,11 @@ Future eventInfo(String userTitle, String userLocation, String userStartDate,
   var result = await https.post(url, body: body, headers: headers);
   print(result.body);
   // If the login was not successful, we return a String so we can send an error message to the user
+  var json = jsonDecode(result.body);
+  // This is how to check the error key
+  if (json.containsKey('detail')){
+    return 'not logged in';
+  }
   if (result.statusCode != 200) {
     return 'Event Creation Failed';
   }
