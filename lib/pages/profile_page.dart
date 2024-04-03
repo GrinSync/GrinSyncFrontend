7bf4a38e6bd361cd8552ad80135793a453936be4
bradleyramsey <bradley.ramsey01@gmail.com>
2024-04-03 18:19:15 -0500

    Logout test

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test_app/constants.dart';
 import 'package:flutter_test_app/pages/login_page.dart';
 import 'package:flutter_test_app/pages/registration_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -13,7 +15,8 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   void initState() {
-    _guestmode = true;  // currently default to guest mode, will be updated to check whether the user is logged in when more login functionality is added
+    _guestmode =
+        true; // currently default to guest mode, will be updated to check whether the user is logged in when more login functionality is added
     super.initState();
   }
 
@@ -22,32 +25,38 @@ class _ProfilePageState extends State<ProfilePage> {
     // if the user is not logged in, display the page with the option to login or register
     if (_guestmode) {
       return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('Welcome to GrinSync!'),
-              const Text('You are currently a guest user.'),
+          body: Center(
+              child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Welcome to GrinSync!'),
+          const Text('You are currently a guest user.'),
+          TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  CupertinoPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+              child: const Text('Login')),
+          TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                      builder: (context) => const RegistrationPage()),
+                );
+              },
+              child: const Text('Register')),
               TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    CupertinoPageRoute(builder: (context) => const LoginPage()),
-                  );
-                },
-                child: const Text('Login')),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    CupertinoPageRoute(builder: (context) => const RegistrationPage()),
-                  );
-                },
-                child: const Text('Register')),
-                ],
-          )
-        ) 
-      );
+              onPressed: () async {
+                    var box = await Hive.openBox(tokenBox);
+                    box.delete('token'); // save token in box
+                    box.close();
+              },
+              child: const Text('LogOut')),
+        ],
+      )));
     }
 
     // if the user is logged in, display the user's profile
