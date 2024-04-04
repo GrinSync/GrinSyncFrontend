d06a39848ed6d78ac2a70b51720a1d4f9858bbab
彭羽鑫 <kevinpyx@163.com>
2024-04-03 20:06:54 -0500

    made guest profile and user profile into seprate pages to further modularize the components

---
+++ lib/pages/guest_profile_page.dart
@@ -0,0 +1,42 @@
+// stateless widget for guest profile page
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test_app/pages/login_page.dart';
+import 'package:flutter_test_app/pages/registration_page.dart';
+
+class GuestProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Welcome to GrinSync!', style: TextStyle(fontSize: 20)),
+            const SizedBox(height: 20),
+            const Text('Please log in or register to access more features.'),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  CupertinoPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+              child: const Text('Login')),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                      builder: (context) => const RegistrationPage()),
+                );
+              },
+              child: const Text('Register')
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/user_profile_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test_app/pages/login_page.dart';
+import 'package:flutter_test_app/pages/registration_page.dart';
+import 'package:flutter_test_app/models/user_models.dart';
+
+class UserProfilePage extends StatelessWidget {
+  final User? user;
+
+  UserProfilePage({Key? key, required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // display info
+            const SizedBox(height: 8),
+            Text('${user?.username}', style: const TextStyle(fontSize: 50, fontWeight: FontWeight.bold, fontFamily: 'Helvetica')),
+            Text(' ${user?.firstName} ${user?.lastName}', style: TextStyle(fontSize: 15, color: Colors.grey[600])),
+            //Text('Email: ${user?.email}'),
+
+            // display options
+            const Divider(color: Colors.black,),
+            TextButton(
+              child: const Text('Edit Profile', style: TextStyle(fontSize: 20)),
+              onPressed: () {
+                // TODO: implement edit profile page
+              },),
+            // const Divider(color: Colors.grey,),
+            TextButton(
+              child: const Text('Events I Follow', style: TextStyle(fontSize: 20)),
+              onPressed: () {
+                // TODO: implement events I follow page
+              },),
+            TextButton(
+              child: const Text('Users/Orgs I Follow', style: TextStyle(fontSize: 20)),
+              onPressed: () {
+                // TODO: implement users/orgs I follow page
+              },),
+            TextButton(
+              child: const Text('Settings', style: TextStyle(fontSize: 20)),
+              onPressed: () {
+                // TODO: implement setting page and functionality
+            },),
+            Expanded(child: SizedBox()), 
+            ElevatedButton(
+              child: const Text('Logout', style: TextStyle(fontSize: 20)),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              onPressed: () {
+                // TODO: implement logout functionality
+              },
+            ),
+        ]),
+      )
+    );
+  }
+}
