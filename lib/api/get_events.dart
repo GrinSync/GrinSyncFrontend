4565d06bd8cb33cb431360e368b689e2ac818b82
彭羽鑫 <kevinpyx@163.com>
2024-04-03 21:12:19 -0500

    added print statements for debugging

--- lib/api/get_events.dart
+++ lib/api/get_events.dart
@@ -9,6 +9,8 @@ import 'package:flutter_test_app/models/event_models.dart';
 Future<List<Event>> getAllEvents() async {
   List<Event> allEvents = [];
 
+  print('Connecting...');
+
   var box = await Hive.openBox(tokenBox);
   var token = box.get('token');
   box.close();
@@ -18,8 +20,11 @@ Future<List<Event>> getAllEvents() async {
   } else {
     headers = {'Authorization': 'Token $token'};
   }
+  print('Fetching events...');
   var url = Uri.parse('https://grinsync.com/api/upcoming');
   var result = await https.get(url, headers: headers);
+
+  print('Parsing JSON response...');
 
   // parse the json response and create a list of Event objects
   // result.body is a list of maps with event information
@@ -27,6 +32,8 @@ Future<List<Event>> getAllEvents() async {
     Event newEvent = Event.fromJson(jsonEvent);
     allEvents.add(newEvent);
   }
+
+  print('Returning events...');
 
   return allEvents;
 }
