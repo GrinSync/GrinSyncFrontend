0a44289e8cf180ef79fc92abf0a39eae3265f762
彭羽鑫 <kevinpyx@163.com>
2024-04-03 21:14:53 -0500

    added more fields for User.fromJson

--- lib/models/user_models.dart
+++ lib/models/user_models.dart
@@ -3,18 +3,22 @@ class User {
   String? username;
   String? firstName;
   String? lastName;
+  String? email;
 
   User({
     this.token,
     this.username,
     this.firstName,
-    this.lastName
+    this.lastName,
+    this.email
   });
 
   factory User.fromJson(json){
     return User(
       username: json['username'],
-
+      firstName: json['first_name'],
+      lastName: json['last_name'],
+      email: json['email']
     );
   } 
 }
