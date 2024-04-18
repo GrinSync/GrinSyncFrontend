7bdc11ae35138d55dbd899e4e66245b3e9844dcd
彭羽鑫 <kevinpyx@163.com>
2024-04-18 15:28:43 -0500

    changed styling

--- lib/pages/guest_event_creation_page.dart
+++ lib/pages/guest_event_creation_page.dart
@@ -5,17 +5,15 @@ import 'package:flutter/material.dart';
 class GuestEventCreationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    return Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('Welcome to GrinSync!', style: TextStyle(fontSize: 20)),
             const SizedBox(height: 20),
-            const Text('Please log in or register to create an event by going to the Profile page.'),
+            const Text('Please log in or register to create an event.'),
           ],
         ),
-      ),
     );
   }
 }
