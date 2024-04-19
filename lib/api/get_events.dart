cfa36f9eb3d7735521349f0f5cf8a7c35aa98c38
彭羽鑫 <kevinpyx@163.com>
2024-04-19 15:56:52 -0500

    added other get event functions and event card classes

--- lib/api/get_events.dart
+++ lib/api/get_events.dart
@@ -1,13 +1,171 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:flutter_test_app/constants.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:http/http.dart' as https;
 import 'package:flutter_test_app/models/event_models.dart';
+import 'package:flutter_test_app/pages/event_details_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test_app/pages/edit_event_page.dart';
+import 'package:flutter_test_app/api/user_authorization.dart';
+
+
+// This event card leads user to the event details page. It's used when user can't edit an event (e.g. on home page and events I follow page)
+class EventCardtoDetails extends StatelessWidget {
+  const EventCardtoDetails({
+    super.key,
+    required this.event,
+  });
+  final Event event;
+
+  @override
+  Widget build(BuildContext context) {
+    var favorited = ValueNotifier(event.isFavoited);
+
+    return ValueListenableBuilder(
+      valueListenable: favorited,
+      builder: (context, value, child) {
+        return Card(
+          child: ListTile(
+            shape: RoundedRectangleBorder(
+              side: BorderSide(color: value? Theme.of(context).colorScheme.primary:Colors.black, width: value? 2:1),
+              borderRadius: BorderRadius.circular(12.0),
+          ),
+            tileColor: value? null:Colors.white,
+            title: Text(
+                event.title ?? 'Null title',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w800)),
+            subtitle: Text(
+                'Location: ${event.location}\nStarts at: ${timeFormat(event.start)}',
+                style: TextStyle(
+                    fontSize: 15, color: Colors.grey[700])),
+            isThreeLine: true,
+            trailing: isLoggedIn()? IconButton(
+              icon: value? Icon(Icons.favorite, color: Theme.of(context).colorScheme.primary):Icon(Icons.favorite_border, color: Theme.of(context).colorScheme.primary),
+              onPressed: () {
+                toggleLikeEvent(event.id);
+                favorited.value = !value;
+              },
+            ):null, // favorite button to favorite an event
+            onTap: () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (context) => EventDetailsPage(event: event),
+                ),
+              );
+            },
+          ),
+        );
+      }
+    );
+  }
+}
+
+// This event card leads user to the event edit page. It's used when user can edit an event (e.g. on events I created page)
+class EventCardtoEdit extends StatelessWidget {
+  const EventCardtoEdit({
+    super.key,
+    required this.event,
+  });
+
+  final Event event;
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Card(
+          child: ListTile(
+            shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.black, width: 1),
+              borderRadius: BorderRadius.circular(12.0),
+          ),
+          tileColor: Colors.white,
+            title: Text(
+                event.title ?? 'Null title',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800)),
+            subtitle: Text(
+                'Location: ${event.location}\nStarts at: ${timeFormat(event.start)}',
+                style: TextStyle(
+                    fontSize: 15, color: Colors.grey[600])),
+            isThreeLine: true,
+            // trailing: Icon(Icons.favorite_border, color: Theme.of(context).colorScheme.primary), // favorite button to favorite an event
+            onTap: () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  //go to event edit page
+                  builder: (context) => EventEditPage(event: event),
+              )
+              );
+            },
+          ),
+        );
+      }
+    }
+    
+
+// This function formats the time string to a more readable format (YYYY-MM-DD HH:MM)
+String timeFormat(String? time) {
+  if (time == null) {
+    return 'Null time';
+  }
+  return '${time.substring(0, 10)} ${time.substring(11, 16)}';
+}
+
+
+Future<void> deleteEvent(int eventId) async {
+  var box = await Hive.openBox(tokenBox);
+  var token = box.get('token');
+  box.close();
+  Map<String, String> headers;
+  if (token == null) {
+    headers = {};
+  } else {
+    headers = {'Authorization': 'Token $token'};
+  }
+
+  var url = Uri.parse('https://grinsync.com/api/deleteEvent');
+  var response = await https.post(url, headers: headers, body: {'id': eventId.toString()});
+
+  if (response.statusCode == 200) {
+    print('Event deleted');
+  } else {
+    print('Failed to delete event');
+  }
+}
+
+
+Future<void> toggleLikeEvent(int eventId) async {
+  var box = await Hive.openBox(tokenBox);
+  var token = box.get('token');
+  box.close();
+  Map<String, String> headers;
+  if (token == null) {
+    headers = {};
+  } else {
+    headers = {'Authorization': 'Token $token'};
+  }
+
+  var url = Uri.parse('https://grinsync.com/api/toggleLikedEvent');
+  var response = await https.post(url, headers: headers, body: {'id': eventId.toString()});
+
+  if (response.statusCode == 200) {
+    print('Event liked/unliked');
+  } else {
+    print('Failed to like/unlike event');
+  }
+}
+
 
 Future<List<Event>> getAllEvents() async {
   List<Event> allEvents = [];
 
-  print('Connecting...');
+  // print('Connecting...');
 
   var box = await Hive.openBox(tokenBox);
   var token = box.get('token');
@@ -22,7 +180,7 @@ Future<List<Event>> getAllEvents() async {
   var url = Uri.parse('https://grinsync.com/api/getAll');
   var result = await https.get(url, headers: headers);
 
-  print('Parsing JSON response...');
+  // print('Parsing JSON response...');
 
   // parse the json response and create a list of Event objects
   // result.body is a list of maps with event information
@@ -31,7 +189,7 @@ Future<List<Event>> getAllEvents() async {
     allEvents.add(newEvent);
   }
 
-  print('Returning events...');
+  // print('Returning events...');
 
   return allEvents;
 }
@@ -95,4 +253,33 @@ Future<List<Event>> getMyEvents() async {
   }
 
   return myEvents;
+}
+
+
+// this function gets the events followed by the current user (assuming the user is logged in)
+Future<List<Event>> getLikedEvents() async {
+  List<Event> likedEvents = [];
+
+  var box = await Hive.openBox(tokenBox);
+  var token = box.get('token');
+  box.close();
+  Map<String, String> headers;
+  if (token == null) {
+    headers = {};
+  } else {
+    headers = {'Authorization': 'Token $token'};
+  }
+
+  var url = Uri.parse('https://grinsync.com/api/getlikedEvents');
+  var result = await https.get(url, headers: headers);
+
+
+  // parse the json response and create a list of Event objects
+  // result.body is a list of maps with event information
+  for (var jsonEvent in jsonDecode(result.body)) {
+    Event newEvent = Event.fromJson(jsonEvent);
+    likedEvents.add(newEvent);
+  }
+
+  return likedEvents;
 }
