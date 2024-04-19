ac7797498309135fa9931915c9c224865a93665f
彭羽鑫 <kevinpyx@163.com>
2024-04-19 15:57:29 -0500

    added id field

--- lib/models/user_models.dart
+++ lib/models/user_models.dart
@@ -1,15 +1,17 @@
 class User {
   String? token;
+  int id;
   String? username;
   String? firstName;
   String? lastName;
   String? email;
 
-  User({this.token, this.username, this.firstName, this.lastName, this.email});
+  User({this.token, this.id = -1, this.username, this.firstName, this.lastName, this.email});
 
   factory User.fromJson(json) {
     return User(
         username: json['username'],
+        id: json['id'],
         firstName: json['first_name'],
         lastName: json['last_name'],
         email: json['email']);
