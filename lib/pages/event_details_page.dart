f8a4393494d519f06f9851a0e2870c4c5ce27489
彭羽鑫 <kevinpyx@163.com>
2024-04-19 15:58:05 -0500

    The like button works now

--- lib/pages/event_details_page.dart
+++ lib/pages/event_details_page.dart
@@ -1,19 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test_app/api/user_authorization.dart';
 import 'package:flutter_test_app/models/event_models.dart';
+import 'package:flutter_test_app/api/get_events.dart';
 
 class EventDetailsPage extends StatelessWidget {
   final Event event;
+
 
   EventDetailsPage({required this.event});
 
   @override
   Widget build(BuildContext context) {
+    var favorited = ValueNotifier(event.isFavoited);
     return Scaffold(
       appBar: AppBar(
         foregroundColor: Colors.white,
         title: const Text('Event Details',
             style: TextStyle(fontWeight: FontWeight.w800)),
         backgroundColor: Theme.of(context).colorScheme.primary,
+        actions: [], //TODO: Add edit button and delete button if user is the organizer
       ),
       body: Container(
         padding: EdgeInsets.all(8.0),
@@ -28,14 +34,19 @@ class EventDetailsPage extends StatelessWidget {
                           fontFamily: 'Helvetica',
                           fontSize: 30,
                           fontWeight: FontWeight.w800))),
-              IconButton(
-                  icon: const Icon(Icons.favorite_border, color: Colors.pink),
-                  tooltip: 'Save the event',
-                  onPressed: () {
-                    // Add event to favorites
-                    // setState
-                    // implement icon logic (Icons.favorite) if event is in favorites (not in here!)
-                  })
+              if (isLoggedIn()) //only shows the like button when the user is logged in
+                ValueListenableBuilder(
+                  valueListenable: favorited,
+                  builder: (context, value, child) {
+                    return IconButton(
+                        icon: value? const Icon(Icons.favorite, color: Colors.pink):Icon(Icons.favorite_border, color: Colors.pink),
+                        tooltip: value? 'Unsave the event':'Save the event',
+                        onPressed: () {
+                          toggleLikeEvent(event.id);
+                          favorited.value = !favorited.value;
+                        });
+                  }
+                )
             ]),
             Text('Organizer',
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800)),
