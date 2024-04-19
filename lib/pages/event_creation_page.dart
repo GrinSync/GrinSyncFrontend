88c129e33558a4a61597c399be28781c2fb01a23
彭羽鑫 <kevinpyx@163.com>
2024-04-19 03:06:16 -0500

    added valueListenableBuilder so they rebuild when login status changes

--- lib/pages/event_creation_page.dart
+++ lib/pages/event_creation_page.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test_app/pages/guest_event_creation_page.dart';
 import 'package:flutter_test_app/pages/user_event_creation_page.dart';
 import 'package:flutter_test_app/global.dart';
+import 'package:flutter_test_app/models/user_models.dart';
 
 class CreatePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-          if (GUESTMODE) {
+    // using valueListenableBuilder so the page rebuilds every time USER changes
+    return ValueListenableBuilder<User?>(
+        valueListenable: USER,
+        builder: (context, user, _) {
+          if (USER.value == null) {
             return GuestEventCreationPage();
           } else {
             return const EventCreationPage();
           }
-        }
+        });
   }
+}
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,80 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test_app/constants.dart';
-import 'package:flutter_test_app/api/user_authorization.dart';
 import 'package:flutter_test_app/models/user_models.dart';
 import 'package:flutter_test_app/pages/guest_profile_page.dart';
 import 'package:flutter_test_app/pages/user_profile_page.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter_test_app/global.dart';
 
-class ProfilePage extends StatefulWidget {
-  @override
-  State<ProfilePage> createState() => _ProfilePageState();
+class ProfilePage extends StatelessWidget {
 
-}
-
-class _ProfilePageState extends State<ProfilePage> {
-  late User? _user;
-
-  // check if the user is logged in
-  // if the user is logged in, set GUESTMODE to false, and set the user to the current user
-  // if the user is not logged in, set GUESTMODE to true, and set the user to null
-  Future<void> checkLoginStatus() async {
-    var box = await Hive.openBox(tokenBox);
-    var token = box.get('token');
-    box.close();
-    if (token == null) {
-      GUESTMODE = true;
-    } else {
-      _user = await getUser(token);
-      if (_user == null) {
-        GUESTMODE = true;
-      } else {
-        GUESTMODE = false;
-      }
-    }
-  }
-
-  refresh() {
-    setState(() {});
-  }
+  ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: checkLoginStatus(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CircularProgressIndicator(),
-              const Text('Loading Profile...'),
-            ],
-          );
-        } else if (snapshot.hasError) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text('Error loading profile'),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    checkLoginStatus();
-                  });
-                },
-                child: const Text('Try again'),
-              ),
-            ],
-          );
+    return ValueListenableBuilder<User?>(
+      valueListenable: USER,
+      builder: (context, user, _) {
+        if (user == null) {
+          return GuestProfilePage();
         } else {
-          if (GUESTMODE) {
-            return GuestProfilePage();
-          } else {
-            return UserProfilePage(user: _user, refreshProfilePage: refresh);
-          }
+          return UserProfilePage();
         }
       },
     );
