bfde7408e2bc3e197270856bcf449659ea6b2178
bradleyramsey <bradley.ramsey01@gmail.com>
2024-04-20 16:59:40 -0500

    Updated Login Page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test_app/api/user_authorization.dart';
+import 'package:flutter_test_app/main.dart';
 import 'package:flutter_test_app/models/user_models.dart';
 import 'package:flutter_test_app/pages/forgot_password_page.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -36,15 +36,58 @@ class _LoginPage extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('GrinSync Login'),
+          // title: const Text('GrinSync Login'),
           backgroundColor: Theme.of(context).colorScheme.primary,
           foregroundColor: Colors.white,
         ),
         body: SingleChildScrollView(
-            child: Container(
-          padding: const EdgeInsets.all(8.0),
+          //padding: const EdgeInsets.all(8.0),
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Container(
+              width: double.infinity,
+              height: 120,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.elliptical(60, 60),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.white),
+                  child: const Icon(
+                    Icons.person,
+                    size: 50,
+                  ),
+                ),
+              ),
+            ),
+        SizedBox(
+          height: 40,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            Text(
+                "Welcome back!",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Sign in to continue.",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+        
             // Create a TextField for the user to enter their username
             TextField(
               controller: _email,
@@ -71,7 +114,7 @@ class _LoginPage extends State<LoginPage> {
                 border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 10),]),),
             // Create Forgot Password Button
             Align(
                 alignment: Alignment.centerRight,
@@ -85,62 +128,64 @@ class _LoginPage extends State<LoginPage> {
                       );
                     },
                     child: const Text('Forgot Password?'))),
-            SizedBox(
-              width: double.infinity,
-              // Create Login Button
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 255, 143, 28),
-                      foregroundColor: Colors.black),
-                  onPressed: () async {
-                    // Authorize user with provided credentials
-                    var auth =
-                        await userAuthentication(_email.text, _password.text);
-                    // If string was returned, login failed.
-                    if (auth.runtimeType == String) {
-                      showDialog(
-                        // Show error message that credentials provided were incorrect
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: const Text('Login Failed'),
-                            content: const SingleChildScrollView(
-                              child: ListBody(
-                                children: <Widget>[
-                                  Text(
-                                      'The Login credentials provided do not match a user.'),
-                                  Text(
-                                      'Please re-enter your credentials and try again.'),
+            Align(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  width: 200,
+                  height: 50,
+                  // Create Login Button
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 218, 41, 28),
+                          foregroundColor: Colors.black),
+                      onPressed: () async {
+                        // Authorize user with provided credentials
+                        var auth = await userAuthentication(
+                            _email.text, _password.text);
+                        // If string was returned, login failed.
+                        if (auth.runtimeType == String) {
+                          showDialog(
+                            // Show error message that credentials provided were incorrect
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text('Login Failed'),
+                                content: const SingleChildScrollView(
+                                  child: ListBody(
+                                    children: <Widget>[
+                                      Text(
+                                          'The Login credentials provided do not match a user.'),
+                                      Text(
+                                          'Please re-enter your credentials and try again.'),
+                                    ],
+                                  ),
+                                ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text('Okay'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
                                 ],
-                              ),
-                            ),
-                            actions: <Widget>[
-                              TextButton(
-                                child: const Text('Okay'),
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                              ),
-                            ],
+                              );
+                            },
                           );
-                        },
-                      );
-                    }
-                    // If user was returned, login succeeded!
-                    else if (auth.runtimeType == User) {
-                      setLoginStatus(); //set USER to current user
-                      Navigator.of(context).pop(); //close login page
-
-                      Fluttertoast.showToast( // show toast message that login was successful
-                        msg: "You are successfully logged in",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.CENTER,
-                        );
-                    }
-                  },
-                  child: const Text('Log in')),
-            )
+                        }
+                        // If user was returned, login succeeded!
+                        else if (auth.runtimeType == User) {
+                          // Open the app home page
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MyApp()),
+                          );
+                        }
+                      },
+                      child: const Text('Log in')),
+                ))
           ]),
-        )));
+        ));
   }
 }
