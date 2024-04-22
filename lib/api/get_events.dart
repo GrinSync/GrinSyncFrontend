61ea3f23db9587758f17c87517cd0356ecab02d5
Nam Do <donam@grinnell.edu>
2024-04-22 08:23:34 -0500

    Fix time parsing for event details

--- lib/api/get_events.dart
+++ lib/api/get_events.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test_app/pages/edit_event_page.dart';
 import 'package:flutter_test_app/api/user_authorization.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-// This event card leads user to the event details page. It's used when user can't edit an event (e.g. on home page and events I follow page)
+/// This event card leads user to the event details page. It's used when user can't edit an event (e.g. on home page and events I follow page)
 class EventCardtoDetails extends StatelessWidget {
   const EventCardtoDetails({
     super.key,
@@ -80,7 +80,7 @@ class EventCardtoDetails extends StatelessWidget {
   }
 }
 
-// This event card leads user to the event edit page. It's used when user can edit an event (e.g. on events I created page)
+/// This event card leads user to the event edit page. It's used when user can edit an event (e.g. on events I created page)
 class EventCardtoEdit extends StatelessWidget {
   const EventCardtoEdit({
     super.key,
@@ -92,56 +92,99 @@ class EventCardtoEdit extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-          child: ListTile(
-            shape: RoundedRectangleBorder(
-              side: BorderSide(color: Colors.black, width: 1),
-              borderRadius: BorderRadius.circular(12.0),
-          ),
-          tileColor: Colors.white,
-            title: Text(
-                event.title ?? 'Null title',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w800)),
-            subtitle: Text(
-                'Location: ${event.location}\nStarts at: ${timeFormat(event.start)}',
-                style: TextStyle(
-                    fontSize: 15, color: Colors.grey[600])),
-            isThreeLine: true,
-            // trailing: IconButton(
-            //   icon: Icon(Icons.delete_outline, color: Theme.of(context).colorScheme.primary),
-            //   onPressed: () {
-            //     deleteEvent(event.id);
-            //   }),
-            onTap: () {
-              Navigator.push(
-                context,
-                CupertinoPageRoute(
-                  //go to event edit page
-                  builder: (context) => EventEditPage(event: event),
-              )
-              );
-            },
-          ),
-        );
-      }
-    }
-    
+      child: ListTile(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(color: Colors.black, width: 1),
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        tileColor: Colors.white,
+        title: Text(event.title ?? 'Null title',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800)),
+        subtitle: Text(
+            'Location: ${event.location}\nStarts at: ${timeFormat(event.start)}',
+            style: TextStyle(fontSize: 15, color: Colors.grey[600])),
+        isThreeLine: true,
+        // trailing: IconButton(
+        //   icon: Icon(Icons.delete_outline, color: Theme.of(context).colorScheme.primary),
+        //   onPressed: () {
+        //     deleteEvent(event.id);
+        //   }),
+        onTap: () {
+          Navigator.push(
+              context,
+              CupertinoPageRoute(
+                //go to event edit page
+                builder: (context) => EventEditPage(event: event),
+              ));
+        },
+      ),
+    );
+  }
+}
 
-// This function formats the time string to a more readable format (YYYY-MM-DD HH:MM)
-String timeFormat(String? time) {
-  if (time == null) {
+/// This function formats the time string to a more readable format (YYYY-MM-DD HH:MM)
+String timeFormat(String? timeString) {
+  if (timeString == null) {
     return 'Null time';
   }
 
-  DateTime dateTimeObj = DateTime.parse(time);
+  DateTime dateTimeObj = DateTime.parse(timeString);
 
+  String year = dateTimeObj.year.toString();
   String month = dateTimeObj.month.toString();
   String day = dateTimeObj.day.toString();
-  String hour = dateTimeObj.hour.toString();
-  String minute = dateTimeObj.minute.toString();
 
-  return '${month}-${day} ${hour}-${minute}';
+  List<String> allMonths = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+
+  switch (month) {
+    case "1":
+      month = allMonths[0];
+    case "2":
+      month = allMonths[1];
+    case "3":
+      month = allMonths[2];
+    case "4":
+      month = allMonths[3];
+    case "5":
+      month = allMonths[4];
+    case "6":
+      month = allMonths[5];
+    case "7":
+      month = allMonths[6];
+    case "8":
+      month = allMonths[7];
+    case "9":
+      month = allMonths[8];
+    case "10":
+      month = allMonths[9];
+    case "11":
+      month = allMonths[10];
+    case "12":
+      month = allMonths[11];
+
+      break;
+    default:
+  }
+
+  // String hour = dateTimeObj.hour.toString();
+  // String minute = dateTimeObj.minute.toString();
+
+  String time = timeString.substring(11, 16);
+
+  return '${time} ${month} ${day}, ${year}';
 }
 
 Future<void> deleteEvent(int eventId) async {
@@ -309,7 +352,6 @@ Future<List<Event>> getLikedEvents() async {
 
   return likedEvents;
 }
-
 
 Future<List<Event>> searchEvents(String query) async {
   List<Event> searchResults = [];
