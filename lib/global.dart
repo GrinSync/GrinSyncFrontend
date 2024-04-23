e70280d529de338e726a03e1b90f634d8b193a7e
彭羽鑫 <kevinpyx@163.com>
2024-04-23 15:20:13 -0500

    dart formated

--- lib/global.dart
+++ lib/global.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test_app/models/user_models.dart';
 
-ValueNotifier<User?> USER = ValueNotifier<User?>(
-    null); //please don't change this variable manually; always use setLoginStatus() to change this variable; when you want to access the current user, use USER.value
+ValueNotifier<User?> USER = ValueNotifier<User?>(null); //please don't change this variable manually; always use setLoginStatus() to change this variable; when you want to access the current user, use USER.value
