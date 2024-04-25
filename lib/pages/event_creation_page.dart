df7f12a31576fa365b2b2056f719efa5ddd0a1a7
Sam Chu <sevenc@gmail.com>
2024-04-25 13:38:08 -0500

    documentation for create page

--- lib/pages/event_creation_page.dart
+++ lib/pages/event_creation_page.dart
@@ -4,24 +4,26 @@ import 'package:flutter_test_app/pages/user_event_creation_page.dart';
 import 'package:flutter_test_app/global.dart';
 import 'package:flutter_test_app/models/user_models.dart';
 
-// Create a page that does not have a state since it does not need to be modified
+/// Creates a stateless page that decides whether to show the Guest or User Event Creation Page based on logged in status. 
+// This page that does not have a state since it does not need to be modified.
 // This page isn't actually seen by users. It just exists behind the scenes
-// and redirects to the proper page that users will see depednding on whether they
-// are logged in or not
+// and redirects to the proper page that users will see depending on whether they
+// are logged in or not.
 class CreatePage extends StatelessWidget {
-  const CreatePage({super.key}); // Constructor
+  const CreatePage({super.key}); // This is the default constructor.
 
   @override
   Widget build(BuildContext context) {
-    // Using valueListenableBuilder so the page rebuilds every time USER changes
+    // We are using ValueListenableBuilder here so the page rebuilds every time the variable USER changes.
+    // Variable USER is null when the user is not logged in. Otherwise, the user is logged in.
     return ValueListenableBuilder<User?>(
         valueListenable: USER,
         builder: (context, user, _) {
-          // If the user is not logged in, go to the Guest Event Creation page
+          // If the user is not logged in, go to the Guest Event Creation page.
           if (USER.value == null) {
-            return GuestEventCreationPage();
+            return const GuestEventCreationPage();
           }
-          // Otherwise, the user is logged in, so they can see the User Event Creation page
+          // Otherwise, the user is logged in, so they can see the User Event Creation page.
           else {
             return const UserEventCreationPage();
           }
