44097dd8fd591140c7f8fe5b1f6bc797c0bc89ca
Sam Chu <sevenc@gmail.com>
2024-04-25 12:42:47 -0500

    add documentation to guest event creation page

--- lib/pages/guest_event_creation_page.dart
+++ lib/pages/guest_event_creation_page.dart
@@ -1,14 +1,18 @@
-// Stateless widget for Guest Event Creation page
-
 import 'package:flutter/material.dart';
 
+/// Creates a stateless page for the Guest Event Creation page. 
+// This page cannot be changed by the user, which is OK because we just want to 
+// display a message here. 
 class GuestEventCreationPage extends StatelessWidget {
-  const GuestEventCreationPage({super.key});
+  const GuestEventCreationPage({super.key}); // This is the default constructor. 
+
   @override
+  // We build the widget here. 
   Widget build(BuildContext context) {
     return const Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center, // Align text in the center of the page. 
+        // Display the text. 
         children: <Widget>[
           Text('Welcome to GrinSync!', style: TextStyle(fontSize: 20)),
           SizedBox(height: 20),
