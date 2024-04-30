9e8c9f2e5c1cdc241893aed93cf6ddbc21b1837c
Bradley Ramsey <bradley.ramsey01@gmail.com>
2024-04-29 19:35:35 -0500

    Login page test

--- test/login_widget_test.dart
+++ test/login_widget_test.dart
@@ -1,29 +1,48 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_test_app/pages/login_page.dart';
+import 'package:flutter_test_app/main.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() {
+  Box box;
+      setUp(() async {
+      final temp = await Directory.systemTemp.createTemp();
+      Hive.init(temp.path);
+      box = await Hive.openBox('test-box');
+      await box.clear();
+    });
   testWidgets('Test the Login Page populated correctly',
       (WidgetTester tester) async {
     // Create the widget by telling the tester to build it.
-    await tester.pumpWidget(const LoginPage());
+    await tester.pumpWidget(const MyApp());
+
+    // Tap on the profile icon once on the home screen
+    await tester.tap(find.byIcon(Icons.person));
+
+    // Rebuild the widget after the state has changed.
+    await tester.pump();
+
+    // Tap on the Login Button on the profile page
+    await tester.tap(find.widgetWithText(TextButton, 'Login'));
+
+    // Rebuild the widget after the state has changed.
+    await tester.pumpAndSettle();
 
     // We can use this test widget to test different aspects of our page
     // We do this below by checking if different fields populated correctly
 
-    // Check the number of text fields that appeared on the page
-    var numTextFields = find.byType(TextField);
-
     // Check the number of buttons that appeared on the page
     var numButtons = find.byType(ElevatedButton);
 
-    // Test that the Title populated
-    expect(find.text('GrinSync Login'), findsOneWidget);
+    // Check the number of text fields that appeared on the page
+    var numTextFields = find.byType(TextField);
 
-    // Test that both Text Fields populated
-    expect(numTextFields, findsNWidgets(2));
-
-    // Test that the Login Button populated
+    // Test that the Login button populated
     expect(numButtons, findsNWidgets(1));
+
+    // Test that all Text Fields populated
+    expect(numTextFields, findsNWidgets(2));
   });
 }
