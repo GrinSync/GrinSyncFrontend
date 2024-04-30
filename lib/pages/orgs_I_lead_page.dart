2cee36947e9aee2090193feda8b3604e47a0efe0
彭羽鑫 <kevinpyx@163.com>
2024-04-30 17:33:26 -0500

    Added MyOrgsPage to display user's organizations

---
+++ lib/pages/orgs_I_lead_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test_app/global.dart';
+import 'package:flutter_test_app/pages/connect_org_page.dart';
+import 'package:flutter_test_app/pages/org_details_page.dart';
+
+class MyOrgsPage extends StatelessWidget {
+  final List<String> studentOrgs = List<String>.from(STUDENTORGS);
+
+  // constructor
+  MyOrgsPage({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Organizations', style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            tooltip: 'Connect to a new student organization',
+            onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ConnectOrgPage()));
+            }),
+        ],
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: Color.fromARGB(255, 235, 230, 229)),
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.separated(
+          separatorBuilder: (context, index) => Divider(),
+          itemCount: studentOrgs.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(studentOrgs[index], style: TextStyle(fontSize: 20, fontFamily: 'Helvetica', fontWeight: FontWeight.bold)),
+              subtitle: Text('[example@studentorg.grinnell.edu]', style: TextStyle(fontSize: 15)),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const OrgDetailsPage()));
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
