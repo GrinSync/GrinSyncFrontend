97aa50dd1fb1bad7007b5fc29af493212f0f38ad
彭羽鑫 <kevinpyx@163.com>
2024-04-30 17:26:54 -0500

    made ALLTAGS final

--- lib/global.dart
+++ lib/global.dart
@@ -6,10 +6,13 @@ var BOX;
 ValueNotifier<User?> USER = ValueNotifier<User?>(
     null); //please don't change this variable manually; always use setLoginStatus() to change this variable; when you want to access the current user, use USER.value
 
-List<String> ALLTAGS =
+final List<String> ALLTAGS =
     []; // this will be a string of all tags separated by commas
 List<String> PREFERREDTAGS =
     []; // this will be a string of all preferred tags separated by commas
+// important: if you are will use the PREFERREDTAGS variable, make sure to use getPreferredTags() and getAllTags() functions in tags.dart
+// if you do yourList = PREFERREDLIST, you could be modifying the global variable unintentionally (learned from real experience ;_;)
+
 List<String> STUDENTORGS =
     []; // this list will contain student org names that the user is linked with
 List<int> ORGIDS =
