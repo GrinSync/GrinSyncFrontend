3cd933c96358f74c8432acc3e339b06917ce17ea
Bradley Ramsey <bradley.ramsey01@gmail.com>
2024-05-11 12:03:27 -0500

    User auth and Orgs I lead Docs

--- lib/api/user_authorization.dart
+++ lib/api/user_authorization.dart
@@ -20,56 +20,54 @@ Future<dynamic> userAuthentication(String username, String password) async {
   if (result.statusCode == 200) {
     Map json = jsonDecode(result.body);
     String token = json['token'];
-    // var box = await Hive.openBox(tokenBox);
     BOX.put('token', token); // save token in box
-    // box.close();
-    User? user = await getUser(token);
+    User? user = await getUser(token); // Get the user now that we have logged in
 
     // initialize the preferred tags and student orgs
     await setPrefferedTags();
     await setStudentOrgs();
 
-    return user;
+    return user; // Return the user
   } else {
     return 'Login Failed';
   }
 }
 
+/// Get the user given the users token
 Future<User?> getUser(String token) async {
   var url = Uri.parse('https://grinsync.com/api/getUser'); // url to send info
   var res = await http.get(url, headers: {'Authorization': 'Token $token'});
-  if (res.statusCode == 200) {
+  if (res.statusCode == 200) { // If getting user succeeded
     var json = jsonDecode(res.body);
-    User user = User.fromJson(json);
+    User user = User.fromJson(json); // Save user
     user.token = token;
-    return user;
+    return user; // Return the user
   } else {
     return null;
   }
 }
 
-// Set login status based on if the user is logged in,
-// if the user is logged in, set USER to the current user
-// if the user is not logged in, set USER to null
+/// Set login status based on if the user is logged in, if the user is logged in, 
+/// set USER to the current user, if the user is not logged in, set USER to null
 Future<void> setLoginStatus() async {
-  // var box = await Hive.openBox(tokenBox);
-  var token = BOX.get('token');
-  if (token == null) {
+  var token = BOX.get('token'); // Get the token
+  if (token == null) { // If there is no token, the user is not logged in, so do not get a user
     USER.value = null;
-  } else {
-    USER.value = await getUser(token);
+  } else { // If a token exists, get the user
+    USER.value = await getUser(token); 
   }
 }
 
-// check if the user is logged in
+/// check if the user is logged in
 bool isLoggedIn() {
   return USER.value != null;
 }
 
+/// Register the user
 Future<dynamic> registerUser(String firstName, String lastName, String email,
     String password, String confirmPassword, SingingCharacter? accType) async {
   String account;
-  switch (accType) {
+  switch (accType) { // Check the account type of the user
     case SingingCharacter.student:
       account = 'STU';
     case SingingCharacter.faculty:
@@ -79,6 +77,7 @@ Future<dynamic> registerUser(String firstName, String lastName, String email,
     case null:
       account = 'COM';
   }
+  /// Store the user data in a map to pass to the backend
   Map<String, String> data = {
     'first_name': firstName,
     'last_name': lastName,
@@ -86,6 +85,8 @@ Future<dynamic> registerUser(String firstName, String lastName, String email,
     'password': password,
     'type': account
   };
+
+  // Check that both password fields are equal
   if (confirmPassword != password) {
     return null;
   }
@@ -93,26 +94,15 @@ Future<dynamic> registerUser(String firstName, String lastName, String email,
       Uri.parse('https://grinsync.com/api/create/user'); // url to send info
   var res = await http.post(url, body: data);
   if (res.statusCode == 200) {
-    //var json = jsonDecode(res.body);
-    // String token = json['key'];
-    // var userAttempt = await getUser(token);
-    // if (userAttempt != null) {
-    //   User user = userAttempt;
-    //   return user;
-    // } else {
-    //   return null;
-    // }
-    return 'Login Success';
+    return 'Login Success'; // Return success if account was created successfully
   } else {
-    return null;
+    return null; // Return null if account creation failed
   }
 }
 
-// Calling this function logs the user out
+/// Calling this function logs the user out
 Future<void> logout() async {
-  //var box = await Hive.openBox(tokenBox);
   BOX.delete('token'); // save token in box
-  // box.close(); //box will be closed in setLoginStatus
   setLoginStatus(); // sets the global variable USER to null
   clearPrefferedTags(); // clears the preferred tags
   clearOrgs(); // clears the student orgs
@@ -125,31 +115,22 @@ Future<void> logout() async {
   );
 }
 
-Future<dynamic> passwordReset(String email) async {
-  // body stores the username and password in a single map data structure
-  Map body = {
-    'username': email,
-  };
-  var url =
-      Uri.parse('https://grinsync.com/api/password_reset/'); // url to send info
-  await http.post(url, body: body);
-}
-
+/// Return the user by their user ID
 Future<User?> getUserByID(int id) async {
-  var token = BOX.get('token');
-
+  var token = BOX.get('token'); // Get the user token
+  // Map to hold token to pass to backend
   Map<String, String> headers;
-  if (token == null) {
+  if (token == null) { // If token is null, do not pass it to the backend
     headers = {};
-  } else {
+  } else { // If there is a token, store it in this map
     headers = {'Authorization': 'Token $token'};
   }
 
   var url = Uri.parse('https://grinsync.com/api/getUser?id=$id');
   var result = await http.get(url, headers: headers);
-  if (result.statusCode == 200) {
+  if (result.statusCode == 200) { // Getting user succeeded, store user
     return User.fromJson(jsonDecode(result.body));
-  } else {
+  } else { // If getting user failed, return null;
     return null;
   }
 }
