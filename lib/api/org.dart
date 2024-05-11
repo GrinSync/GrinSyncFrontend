9f60abaff3e08053df488ead012bf906a426fc6f
Bradley Ramsey <bradley.ramsey01@gmail.com>
2024-05-10 22:58:37 -0500

    Create Org Documentation

--- lib/api/org.dart
+++ lib/api/org.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_test_app/global.dart';
 import 'package:http/http.dart' as http;
 
+/// Function to create pass information to backend to create a student org
 Future<dynamic> createOrg(String email, String orgName, String orgCode) async {
-  // body stores the username and password in a single map data structure
+  // body stores the org name, org alias, and email in a single map data structure
   Map body = {'name': orgName, 'alias': orgCode, 'email': email};
-  var token = BOX.get('token');
+  var token = BOX.get('token'); // Get the users token
   Map<String, String> headers;
   if (token == null) {
+    // If no token, do not pass a token to the backend
     headers = {};
   } else {
+    // If token exists, store it in this header map to pass to backend
     headers = {'Authorization': 'Token $token'};
   }
   var url =
@@ -16,9 +19,8 @@ Future<dynamic> createOrg(String email, String orgName, String orgCode) async {
   var result = await http.post(url, headers: headers, body: body);
 
   if (result.statusCode == 200) {
-    return 'Success';
+    return 'Success'; // Return Success if org was created successfully
   } else {
-    print(result.body);
-    return null;
+    return null; // Return null if org was not created
   }
 }
