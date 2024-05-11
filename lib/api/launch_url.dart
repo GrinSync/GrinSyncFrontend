7ae6f949f5e36a95825dad8e7c3500adecaaffb8
Bradley Ramsey <bradley.ramsey01@gmail.com>
2024-05-11 11:28:33 -0500

    Launch url docs

--- lib/api/launch_url.dart
+++ lib/api/launch_url.dart
@@ -2,8 +2,9 @@ import 'dart:io';
 
 import 'package:url_launcher/url_launcher.dart';
 
+/// Function to launch the passed url from the app
 Future<void> urlLauncher(Uri url) async {
-  if (!await launchUrl(url)) {
+  if (!await launchUrl(url)) { // If url launch failed, throw exception
     throw Exception('Could not launch $url');
   }
 }
@@ -12,18 +13,16 @@ Future<void> urlLauncher(Uri url) async {
 // for more google map schemes: https://developers.google.com/maps/documentation/urls/ios-urlscheme#swift_4
 class MapUtils {
   MapUtils._();
-
+  /// Function to launch google maps on native platform
   static Future<void> launchGoogleMaps(
       double destinationLatitude, double destinationLongitude) async {
-    // const double destinationLatitude= 31.5204;
-    // const double destinationLongitude = 74.3587;
-    var googleUrl;
+    Uri googleUrl; 
 
-    if (Platform.isAndroid) {
+    if (Platform.isAndroid) { // If on android, use native google maps to navigate
       googleUrl = Uri(scheme: "google.navigation", queryParameters: {
         'q': '$destinationLatitude, $destinationLongitude'
       });
-    } else if (Platform.isIOS) {
+    } else if (Platform.isIOS) { // If on ios, open google maps
       googleUrl = Uri(scheme: "comgooglemaps", path: '/', queryParameters: {
         'daddr': '$destinationLatitude, $destinationLongitude'
       });
@@ -31,33 +30,29 @@ class MapUtils {
       throw 'Unsupported platform';
     }
 
-    if (await canLaunchUrl(googleUrl)) {
+    if (await canLaunchUrl(googleUrl)) { // if url can be launched from the app, launch the url
       await launchUrl(googleUrl);
-    } else {
-      print('An error occurred');
     }
   }
 }
 
 class MailUtils {
   MailUtils._();
-
+  /// Function to open mail to email host of event
   static Future<void> contactHost(hostEmail, eventTitle) async {
     // All spaces are replaced with %20 to avoid "+"
     String subject = 'Regarding $eventTitle'.replaceAll(' ', '%20');
     String body = 'Hello,\n I am interested in your event $eventTitle.'
         .replaceAll(' ', '%20');
 
-    Uri url = Uri(
+    Uri url = Uri( // url to open email from app
       scheme: 'mailto',
       path: hostEmail,
       query: 'subject=$subject&body=$body',
     );
 
-    if (await canLaunchUrl(url)) {
+    if (await canLaunchUrl(url)) { // if url can be launched from the app, launch the url
       await launchUrl(url);
-    } else {
-      print('An error occurred');
     }
   }
 }
